70f76e91f43d531c2482f0a64df4cddf06a83cb5
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2021-05-23 03:23:15 +0200

    Cleanup and fixes

--- emoji.py
+++ emoji.py
@@ -1,160 +1,190 @@
 __module_name__ = "emoji"
-__module_version__ = "1.1"
+__module_version__ = "2.0"
 __module_description__ = "Emoji support for HexChat"
-
-import re
 
 import emojis
 import hexchat
+import re
 
+#############################################
+#                                           #
+#               Configuration               #
+#                                           #
+#############################################
 
-# you can use /em :emoji:
-# /em can simulate say or me
-COMMAND_EM_SIMULATE = 'say'
+# EM command (/em <text>) needs to mimic another command
+# It should mimic SAY or ME, other commands may not work
+# SAY command: "say"
+# ME command:  "me"
+COMMAND_EM_SIMULATES = "say"
 
-# Auto translate /say message to emojis if any
+# Override SAY command to translate the message to emojis if any
+# Note: This will trigger when the user hits enter or use /say
+# Enabled:  True
+# Disabled: False
 OVERRIDE_SAY = True
 
-# Auto translate /me message to emojis if any
+# Override ME command to translate the message to emojis if any
+# Enabled:  True
+# Disabled: False
 OVERRIDE_ME = True
 
 # Convert emoticons into emojis as in Slack
+# See table conversion in EMOTICON_TO_EMOJI_ALIASES
+# Enabled:  True
+# Disabled: False
 EMOTICON_TO_EMOJI = True
 
-
+# Emoticon to Emoji aliases to use if EMOTICON_TO_EMOJI is True
+# This is not an extensive list, but includes most popular ones
+# Remember to escape backslashs with another backslash: \ --> \\
 EMOTICON_TO_EMOJI_ALIASES = {
-    '<3': ':heart:',
-    '</3': ':broken_heart:',
-    '8)': ':sunglasses:',
-    '8-)': ':sunglasses:',
-    'D:': ':anguished:',
-    ':\'(': ':cry:',
-    ':o)': ':monkey_face:',
-    ':*': ':kissing_heart:',
-    ':-*': ':kissing_heart:',
-    '=)': ':smiley:',
-    '=-)': ':smiley:',
-    ':D': ':smile:',
-    ':-D': ':smile:',
-    ';)': ':wink:',
-    ';-)': ':wink:',
-    ':>': ':laughing:',
-    ':->': ':laughing:',
-    ':|': ':neutral_face:',
-    ':-|': ':neutral_face:',
-    ':o': ':open_mouth:',
-    ':-o': ':open_mouth:',
-    '>:(': ':angry:',
-    '>:-(': ':angry:',
-    ':)': ':slightly_smiling_face:',
-    '(:': ':slightly_smiling_face:',
-    ':-)': ':slightly_smiling_face:',
-    ':(': ':disappointed:',
-    '):': ':disappointed:',
-    ':-(': ':disappointed:',
-    ':/': ':confused:',
-    ':-/': ':confused:',
-    ':\\': ':confused:',
-    ':-\\': ':confused:',
-    ':P': ':stuck_out_tongue:',
-    ':p': ':stuck_out_tongue:',
-    ':-p': ':stuck_out_tongue:',
-    ':b': ':stuck_out_tongue:',
-    ':-b': ':stuck_out_tongue:',
-    ';P': ':stuck_out_tongue_winking_eye:',
-    ';p': ':stuck_out_tongue_winking_eye:',
-    ';-p': ':stuck_out_tongue_winking_eye:',
-    ';b': ':stuck_out_tongue_winking_eye:',
-    ';-b': ':stuck_out_tongue_winking_eye:',
+    "<3": ":heart:",
+    "</3": ":broken_heart:",
+    "8)": ":sunglasses:",
+    "8-)": ":sunglasses:",
+    "D:": ":anguished:",
+    ":'(": ":cry:",
+    ":o)": ":monkey_face:",
+    ":*": ":kissing_heart:",
+    ":-*": ":kissing_heart:",
+    "=)": ":smiley:",
+    "=-)": ":smiley:",
+    ":D": ":smile:",
+    ":-D": ":smile:",
+    ";)": ":wink:",
+    ";-)": ":wink:",
+    ":>": ":laughing:",
+    ":->": ":laughing:",
+    ":|": ":neutral_face:",
+    ":-|": ":neutral_face:",
+    ":o": ":open_mouth:",
+    ":-o": ":open_mouth:",
+    ">:(": ":angry:",
+    ">:-(": ":angry:",
+    ":)": ":slightly_smiling_face:",
+    "(:": ":slightly_smiling_face:",
+    ":-)": ":slightly_smiling_face:",
+    ":(": ":disappointed:",
+    "):": ":disappointed:",
+    ":-(": ":disappointed:",
+    ":/": ":confused:",
+    ":-/": ":confused:",
+    ":\\": ":confused:",
+    ":-\\": ":confused:",
+    ":P": ":stuck_out_tongue:",
+    ":p": ":stuck_out_tongue:",
+    ":-p": ":stuck_out_tongue:",
+    ":b": ":stuck_out_tongue:",
+    ":-b": ":stuck_out_tongue:",
+    ";P": ":stuck_out_tongue_winking_eye:",
+    ";p": ":stuck_out_tongue_winking_eye:",
+    ";-p": ":stuck_out_tongue_winking_eye:",
+    ";b": ":stuck_out_tongue_winking_eye:",
+    ";-b": ":stuck_out_tongue_winking_eye:",
 }
 
 
-RE_GROUPS = '|'.join([r'({0}(?=\s|$))'.format(re.escape(x)) for x in EMOTICON_TO_EMOJI_ALIASES])
+##################################################################
+#                            WARNING!                            #
+#                                                                #
+# Do not edit below this area unless you know what you are doing #
+#                                                                #
+#                              See:                              #
+#  https://hexchat.readthedocs.io/en/latest/script_python.html   #
+##################################################################
+
+
+def build_re_groups():
+    """
+        Build a regex string based on the emoticon to emoji aliases
+
+        The regex includes a lookahed to avoid replacing characters
+        inside a string, such as URLs
+    """
+    for alias in EMOTICON_TO_EMOJI_ALIASES:
+        escaped = re.escape(alias)
+        yield rf"({escaped}(?=\s|$))"
+
+
+def get_emoji_alias(match):
+    return EMOTICON_TO_EMOJI_ALIASES[match.group()]
+
+
+RE_GROUPS = "|".join(build_re_groups())
 RE_EMOTICON_TO_EMOJI_ALIASES = re.compile(RE_GROUPS)
 
 
-def emojize(msg):
-    if EMOTICON_TO_EMOJI:
-        msg = RE_EMOTICON_TO_EMOJI_ALIASES.sub(lambda match: EMOTICON_TO_EMOJI_ALIASES[match.group()], msg)
+def emojize(msg, emoticon_to_emoji=EMOTICON_TO_EMOJI):
+    """ Emojize messages and convert emoticons to emojis if enabled """
+    if emoticon_to_emoji:
+        msg = RE_EMOTICON_TO_EMOJI_ALIASES.sub(get_emoji_alias, msg)
 
     return emojis.encode(msg)
 
 
-def msg_cb(words, word_eol, userdata):
-    msg = word_eol[0]
-    emojized = emojize(msg)
+def say_cb(words, word_eol, userdata):
+    """ Override defaut say command callback to emojize messages """
+    if len(word_eol) > 0:
+        msg = word_eol[0]
+        emojized = emojize(msg)
 
-    if msg != emojized:
-        hexchat.command('say {0}'.format(emojized))
-        return hexchat.EAT_HEXCHAT
+        if msg != emojized:
+            hexchat.command(f"say {emojized}")
+            return hexchat.EAT_HEXCHAT
 
     return hexchat.EAT_NONE
 
 
-def say_cb(words, word_eol, userdata):
-    if len(word_eol) > 1:
-        msg = word_eol[1]
-        emojized = emojize(msg)
-
-        if msg != emojized:
-            hexchat.command('say {0}'.format(emojized))
-            return hexchat.EAT_HEXCHAT
-
-        return hexchat.EAT_NONE
-
-    hexchat.command('help em')
-    return hexchat.EAT_ALL
-
-
 def me_cb(words, word_eol, userdata):
+    """ Override defaut me command callback to emojize messages """
     if len(word_eol) > 1:
         msg = word_eol[1]
         emojized = emojize(msg)
 
         if msg != emojized:
-            hexchat.command('me {0}'.format(emojized))
+            hexchat.command(f"me {emojized}")
             return hexchat.EAT_HEXCHAT
 
-        return hexchat.EAT_NONE
-
-    hexchat.command('help em')
-    return hexchat.EAT_ALL
+    return hexchat.EAT_NONE
 
 
 def em_cb(words, word_eol, userdata):
+    """ EM command callback to emojize messages """
     if len(word_eol) > 1:
         msg = word_eol[1]
         emojized = emojize(msg)
+        cmd = userdata.get("command", "say")
 
-        if COMMAND_EM_SIMULATE in ['say', 'em']:
-            cmd = COMMAND_EM_SIMULATE
-        else:
-            cmd = 'say'
+        hexchat.command(f"{cmd} {emojized}")
+    else:
+        hexchat.command("help em")
 
-        hexchat.command('{0} {1}'.format(cmd, emojized))
-        return hexchat.EAT_HEXCHAT
-
-    hexchat.command('help em')
     return hexchat.EAT_ALL
 
 
 def unload_cb(userdata):
-    print('{0} unloaded.'.format(__module_name__))
+    print(f"{__module_name__} unloaded.")
 
 
-if OVERRIDE_SAY:
-    # override input enter
-    hexchat.hook_command('', msg_cb)
-    # override SAY command
-    hexchat.hook_command('SAY', say_cb)
+def load_plugin():
+    if OVERRIDE_SAY:
+        # override /say command
+        hexchat.hook_command("", say_cb)
 
-if OVERRIDE_ME:
-    # override ME command
-    hexchat.hook_command('ME', me_cb)
+    if OVERRIDE_ME:
+        # override /me command
+        hexchat.hook_command("ME", me_cb)
 
-# EM emojify command
-hexchat.hook_command('EM', em_cb, help='Usage: EM <text>, converts emoji alias into unicode encoded text')
+    # register emojify command (/em <text>)
+    hexchat.hook_command("EM", em_cb,
+        userdata={"command": COMMAND_EM_SIMULATES},
+        help="Usage: EM <text>, converts text with emojis aliases into an unicode encoded text")
 
-hexchat.hook_unload(unload_cb)
+    hexchat.hook_unload(unload_cb)
 
-print('{0} version {1} loaded.'.format(__module_name__, __module_version__))
+    print(f"{__module_name__} version {__module_version__} loaded.")
+
+
+if __name__ == "__main__":
+    load_plugin()
